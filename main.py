130e1d6c541f73128d6a28516b937895d5f47f19
Mahizabin <123229709+Mahizabin@users.noreply.github.com>
2023-06-12 23:34:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#Calculator using python for two numbers
+number1=int(input("Enter the number1 on which operation is to be performed: "))
+number2=int(input("Enter the number2 on which operation is to be performed: "))
+print("The sum of two numbers is:",number1+number2)
+print("The product of two numbers is:",number1*number2)
+print("The difference of two numbers is:",number1-number2)
+print("The division of two numbers is:",number1/number2)
+print("The floor division of two numbers is:",number1//number2)
+print("The modulus of two numbers is:",number1%number2)
+print("The exponential operator on two numbers is:",number1**number2)
+
+
